6be91cb051b07843c519fd835ffb72c33a789e2f
itsalexli <alex.liyou@gmail.com>
2026-01-10 15:46:41 -0500

    Update ContentView.swift

--- DeltaHacks2026/ContentView.swift
+++ DeltaHacks2026/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            
+            .padding()
         }
-        .padding()
     }
 }
 
